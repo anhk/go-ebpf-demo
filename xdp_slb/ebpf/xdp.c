0ba2320e1f720d2b5b2011d1f9301e6df9c71cfd
anhongkui <anhongkui@jd.com>
2023-08-14 10:36:06 +0800

    update

--- xdp_slb/ebpf/xdp.c
+++ xdp_slb/ebpf/xdp.c
@@ -2,6 +2,7 @@
 #include <linux/if_ether.h> // for struct ethhdr
 #include <linux/in.h>       // for IPPROTO_TCP
 #include <linux/ip.h>       // for struct iphdr
+#include <linux/string.h>   // for memset
 #include <linux/tcp.h>      // for struct tcpdhr
 #include "bpf_endian.h"
 #include "bpf_helpers.h"
@@ -41,36 +42,41 @@ static int fib_redirect(struct xdp_md *ctx, struct ethhdr *eth, struct iphdr *ip
     // FIXME: field XdpSlb: program xdp_slb: load program: permission denied: invalid indirect read from stack R2 off
     // -64+13 size 64 (125 line(s) omitted)
 
-    // struct bpf_fib_lookup fib_params = {
-    //     .family = AF_INET,
-    //     .tos = iph->tos,
-    //     .l4_protocol = iph->protocol,
-    //     .sport = 0,
-    //     .dport = 0,
-    //     .tot_len = bpf_ntohs(iph->tot_len),
-    //     .ipv4_src = lip,
-    //     .ipv4_dst = rip,
-    //     .ifindex = ctx->ingress_ifindex,
-    // };
+    struct bpf_fib_lookup fib_params;
+    memset(&fib_params, 0, sizeof(fib_params));
 
-    // int rc = bpf_fib_lookup(ctx, &fib_params, sizeof(fib_params), 0);
-    // bpf_printk("bpf_fib_lookup return %d", rc);
+    fib_params.family = AF_INET;
+    fib_params.tos = iph->tos;
+    fib_params.l4_protocol = iph->protocol;
+    fib_params.tot_len = bpf_ntohs(iph->tot_len);
+    fib_params.ipv4_src = lip;
+    fib_params.ipv4_dst = rip;
+    fib_params.ifindex = ctx->ingress_ifindex;
 
-    // switch (rc) {
-    // case BPF_FIB_LKUP_RET_SUCCESS: /* lookup successful */
-    //     break;
-    // case BPF_FIB_LKUP_RET_BLACKHOLE:   /* dest is blackholed; can be dropped */
-    // case BPF_FIB_LKUP_RET_UNREACHABLE: /* dest is unreachable; can be dropped */
-    // case BPF_FIB_LKUP_RET_PROHIBIT:    /* dest not allowed; can be dropped */
-    //     return XDP_DROP;
-    // case BPF_FIB_LKUP_RET_NOT_FWDED:    /* packet is not forwarded */
-    // case BPF_FIB_LKUP_RET_FWD_DISABLED: /* fwding is not enabled on ingress */
-    // case BPF_FIB_LKUP_RET_UNSUPP_LWT:   /* fwd requires encapsulation */
-    // case BPF_FIB_LKUP_RET_NO_NEIGH:     /* no neighbor entry for nh */
-    // case BPF_FIB_LKUP_RET_FRAG_NEEDED:  /* fragmentation required to fwd */
-    // default:
-    //     break;
-    // }
+    int rc = bpf_fib_lookup(ctx, &fib_params, sizeof(fib_params), 0);
+    bpf_printk("bpf_fib_lookup return %d", rc);
+    bpf_printk(" - src: %02x:%02x:%02x:%02x:%02x:%02x, dst: %02x:%02x:%02x:%02x:%02x:%02x", fib_params.smac[0],
+               fib_params.smac[1], fib_params.smac[2], fib_params.smac[3], fib_params.smac[4], fib_params.smac[5],
+               fib_params.dmac[0], fib_params.dmac[1], fib_params.dmac[2], fib_params.dmac[3], fib_params.dmac[4],
+               fib_params.dmac[5]);
+
+    switch (rc) {
+    case BPF_FIB_LKUP_RET_SUCCESS: /* lookup successful */
+    memcpy(eth->h_dest, fib_params.dmac, ETH_ALEN);
+    memcpy(eth->h_source, fib_params.smac, ETH_ALEN);
+    return XDP_TX;
+    case BPF_FIB_LKUP_RET_BLACKHOLE:   /* dest is blackholed; can be dropped */
+    case BPF_FIB_LKUP_RET_UNREACHABLE: /* dest is unreachable; can be dropped */
+    case BPF_FIB_LKUP_RET_PROHIBIT:    /* dest not allowed; can be dropped */
+        return XDP_DROP;
+    case BPF_FIB_LKUP_RET_NOT_FWDED:    /* packet is not forwarded */
+    case BPF_FIB_LKUP_RET_FWD_DISABLED: /* fwding is not enabled on ingress */
+    case BPF_FIB_LKUP_RET_UNSUPP_LWT:   /* fwd requires encapsulation */
+    case BPF_FIB_LKUP_RET_NO_NEIGH:     /* no neighbor entry for nh */
+    case BPF_FIB_LKUP_RET_FRAG_NEEDED:  /* fragmentation required to fwd */
+    default:
+        break;
+    }
 
     return XDP_DROP;
 }
