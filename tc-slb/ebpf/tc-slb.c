8d35a6e228105cc73a2d5a674e56ad5b98cb4b67
anhongkui <anhongkui@jd.com>
2023-08-15 12:40:25 +0800

    almost done

--- tc-slb/ebpf/tc-slb.c
+++ tc-slb/ebpf/tc-slb.c
@@ -12,14 +12,72 @@
 char __license[] SEC("license") = "GPL";
 
 #define AF_INET 2
-#define IP_CSUM_OFFSET (sizeof(struct ethhdr) + offsetof(struct iphdr, check))
+#define IP_CSUM_OFFSET (ETH_HLEN + offsetof(struct iphdr, check))
+
 #define UDP_CSUM_OFFSET (sizeof(struct ethhdr) + sizeof(struct iphdr) + offsetof(struct udphdr, check))
 #define TCP_CSUM_OFFSET (sizeof(struct ethhdr) + sizeof(struct iphdr) + offsetof(struct tcphdr, check))
+
+unsigned long long load_byte(void *skb, unsigned long long off) asm("llvm.bpf.load.byte");
+unsigned long long load_half(void *skb, unsigned long long off) asm("llvm.bpf.load.half");
+unsigned long long load_word(void *skb, unsigned long long off) asm("llvm.bpf.load.word");
 
 __be32 VIP = 0x2540a8c0; // ==> 192.168.64.37
 __u16 VPORT = 0x0F27;    // ==> 9999
 __be32 BIP = 0x2640a8c0; // ==> 192.168.64.38
 __u16 BPORT = 0x1600;    // ==> 22
+__be32 SIP = 0x140a8c0;  // 192.168.64.1
+
+int fixup_ip_checksum(struct __sk_buff *skb)
+{
+    if (load_half(skb, offsetof(struct ethhdr, h_proto)) != ETH_P_IP)
+        return 0;
+
+    // Calculate one's complement sum of IP header except checksum word at
+    // offset 24.
+    __u64 checksum = 0;
+    checksum += load_half(skb, 14);
+    checksum += load_word(skb, 16);
+    checksum += load_word(skb, 20);
+    checksum += load_word(skb, 26);
+    checksum += load_word(skb, 30);
+
+    checksum = (checksum & 0xffff) + (checksum >> 16);
+    checksum = (checksum & 0xffff) + (checksum >> 16);
+    checksum = (checksum & 0xffff) + (checksum >> 16);
+
+    // Write back correct checksum in the packet.
+    __u16 checksum16 = bpf_htons(~checksum);
+
+    bpf_skb_store_bytes(skb, IP_CSUM_OFFSET, &checksum16, 2, 0);
+
+    return 0;
+}
+
+int proxy_ipv4(struct __sk_buff *skb, struct iphdr *iph, struct tcphdr *tcph, __be32 sip, __be32 dip)
+{
+    if (iph == NULL || tcph == NULL) {
+        return TC_ACT_OK;
+    }
+    bpf_printk("%pI4:%d -> %pI4:%d", &iph->saddr, bpf_ntohs(tcph->source), &iph->daddr, bpf_ntohs(tcph->dest));
+    // tcph->dest = BPORT;
+
+    bpf_l4_csum_replace(skb, TCP_CSUM_OFFSET, iph->daddr, dip, BPF_F_PSEUDO_HDR | sizeof(dip));
+    bpf_l3_csum_replace(skb, IP_CSUM_OFFSET, iph->daddr, dip, sizeof(dip));
+
+    bpf_l4_csum_replace(skb, TCP_CSUM_OFFSET, iph->saddr, sip, BPF_F_PSEUDO_HDR | sizeof(sip));
+    bpf_l3_csum_replace(skb, IP_CSUM_OFFSET, iph->saddr, sip, sizeof(sip));
+
+    iph->daddr = dip;
+    iph->saddr = sip;
+
+    bpf_printk("%pI4:%d -> %pI4:%d", &iph->saddr, bpf_ntohs(tcph->source), &iph->daddr, bpf_ntohs(tcph->dest));
+
+    struct bpf_redir_neigh neigh = {
+        .nh_family = AF_INET,
+        .ipv4_nh = dip,
+    };
+    return bpf_redirect_neigh(skb->ifindex /*enp0s1*/, &neigh, sizeof(struct bpf_redir_neigh), 0);
+}
 
 int tc_process_ipv4(struct __sk_buff *skb)
 {
@@ -51,38 +109,10 @@ int tc_process_ipv4(struct __sk_buff *skb)
     struct tcphdr *tcph = data + sizeof(struct ethhdr) + sizeof(struct iphdr);
 
     if (iph->daddr == VIP && tcph->dest == VPORT) {
-        bpf_printk("%pI4:%d -> %pI4:%d", &iph->saddr, bpf_ntohs(tcph->source), &iph->daddr, bpf_ntohs(tcph->dest));
-
-        // __s64 sum = 0;
-        // __be32 old_ip = iph->daddr;
-        // __be32 new_ip = BIP;
-        // sum = bpf_csum_diff(&old_ip, 4, &new_ip, 4, 0);
-        // bpf_l3_csum_replace(skb, IP_CSUM_OFFSET, iph->daddr , BIP, sizeof(BIP));
-        iph->daddr = BIP;
-        // bpf_l4_csum_replace(skb, IP_CSUM_OFFSET, 0, sum, BPF_F_PSEUDO_HDR);
-        // tcph->dest = BPORT;
-
-        // old_ip = iph->saddr;
-        // new_ip = VIP;
-        // sum = bpf_csum_diff(&old_ip, 4, &new_ip, 4, 0);
-        iph->saddr = VIP;
-        // bpf_l3_csum_replace(skb, IP_CSUM_OFFSET, 0, sum, 0);
-        // bpf_l4_csum_replace(skb, IP_CSUM_OFFSET, 0, sum, BPF_F_PSEUDO_HDR);
-        // bpf_l4_csum_replace(skb,TCP_CSUM_OFF, );
-        bpf_printk("%pI4:%d -> %pI4:%d", &iph->saddr, bpf_ntohs(tcph->source), &iph->daddr, bpf_ntohs(tcph->dest));
-        // bpf_skc_lookup_tcp();
-
-        // bpf_set_hash_invalid(skb);
-        // bpf_get_hash_recalc(skb);
-
-        struct bpf_redir_neigh neigh = {
-            .nh_family = AF_INET,
-            .ipv4_nh = BIP,
-        };
-        // memset(&neigh, 0, sizeof(struct bpf_redir_neigh));
-        return bpf_redirect_neigh(skb->ifindex /*enp0s1*/, &neigh, sizeof(struct bpf_redir_neigh), 0);
+        return proxy_ipv4(skb, iph, tcph, VIP, BIP);
     } else if (iph->saddr == BIP && tcph->source == VPORT) {
-        bpf_printk("=> %pI4:%d -> %pI4:%d", &iph->saddr, bpf_ntohs(tcph->source), &iph->daddr, bpf_ntohs(tcph->dest));
+        bpf_printk("==> %pI4:%d -> %pI4:%d", &iph->saddr, bpf_ntohs(tcph->source), &iph->daddr, bpf_ntohs(tcph->dest));
+        return proxy_ipv4(skb, iph, tcph, VIP, SIP);
     } else if (iph->saddr == BIP && tcph->source == BPORT) {
         bpf_printk("=> %pI4:%d -> %pI4:%d", &iph->saddr, bpf_ntohs(tcph->source), &iph->daddr, bpf_ntohs(tcph->dest));
     }
